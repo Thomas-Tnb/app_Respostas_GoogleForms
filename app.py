7190f9bc9945df3bfcff77226de3b8d6cbb9160e
Thomas-Tnb <thomas.jt2005@gmail.com>
2025-02-17 19:49:12 -0300

    Adicionando gráfico de faixa salarial

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
 import streamlit as st  
 import pandas as pd
 
-
 # Acessando a planilha
 url = "https://docs.google.com/spreadsheets/d/1JEsn8iuQw4bXJ4wC1QFsq74GBGQfFz5-Fj9dpIRj4lo/export?format=csv"
 
@@ -9,11 +8,18 @@ url = "https://docs.google.com/spreadsheets/d/1JEsn8iuQw4bXJ4wC1QFsq74GBGQfFz5-F
 # Convertendo para dataframe
 df = pd.read_csv(url)
 
+# Faixa etária 
+if 20 < df["Idade"] <= 35:
+    df["Faixa Etária"] = "Entre 20 e 35 anos"
+elif 35 < df["Idade"] <= 50:
+    df["Faixa Etária"] = "Entre 35 e 50 anos"
+else:
+    df["Faixa Etária"] = "Mais que 50 anos"
+
 # Criando Dashboard no Streamlit
 st.title("Dashboard Shopfono")
 st.write("Dados extraídos do google sheets :")
 st.dataframe(df)
 
-# Estatísticas básicas
-st.subheader("Resumo Estatístico")
-st.write(df.describe())
+
+st.bar_chart(df["Faixa Etária"])
